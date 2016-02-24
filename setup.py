ddbd3a0ab38d56ed28f2eb8aaf0d29771741cfda
Elliot <elbiot@elbitop>
2016-02-24 13:51:27 -0600

    rearrange structure for pip installable

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name = "asanaCRM",
+    version = "0.01",
+    author = "Elliot Hallmark",
+    author_email = "permafacture@gmail.com",
+    description = ("Parse Asana.com projects into a query-able faux database"),
+    license = "GPLv2",
+    keywords = "asana, task management",
+    url = "https://github.com/Permafacture/AsanaCRM",
+    packages=['asanaCRM',],
+)
+
